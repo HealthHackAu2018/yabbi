40cdef16d5067282aaea5451046b9ef1dd162a5b
Will <w.forster.1@gmail.com>
2018-09-15 21:04:44 +1000

    Implemented OrbitControls and made 3dvis.js a bit nicer

--- yabbi/static/scripts/3dvis.js
+++ yabbi/static/scripts/3dvis.js
@@ -1,4 +1,6 @@
-const DEFAULT_POINT_SIZE = 0.03;
+const DEFAULT_POINT_SIZE = 0.06;
+const DIV_ELEMENT_ID = 'threedvis'; // The div for three.js to work in
+const ORBIT_LOOKAT_COORD = new THREE.Vector3(0, 0, 5);
 
 var camera, controls, scene, renderer, points;
 
@@ -18,11 +20,14 @@ function initializePointCloud(scene, arraySize) {
     var points = [];
 
     for (var x = 0; x < arraySize; x++) {
-        var geometry = new THREE.Geometry();
-        geometry.vertices.push(new THREE.Vector3(0,0, x * 0.01));
-        var material = new THREE.PointsMaterial({size: DEFAULT_POINT_SIZE, 
+        /** If performance is inadequate, try using THREE.BufferGeometry
+         * instead of THREE.Geometry which is slower but friendlier */
+        let geometry = new THREE.Geometry();
+        geometry.vertices.push(new THREE.Vector3(4*Math.sin(2*Math.PI*x/200),
+                4 * Math.cos(2*Math.PI*x/200), x * 0.01));
+        let material = new THREE.PointsMaterial({size: DEFAULT_POINT_SIZE,
                 color: 0x0000ff});
-        var point = new THREE.Points(geometry, material);
+        let point = new THREE.Points(geometry, material);
         points.push(point);
         scene.add(point);
     }
@@ -35,26 +40,30 @@ function init() {
     scene.background = new THREE.Color(0xcccccc);
     camera = new THREE.PerspectiveCamera( 75, 
             window.innerWidth/window.innerHeight, 0.1, 1000 );
-    camera.position.z = 5;
+    camera.up.set(0,0,1); // Z axis is up instead of Y
+    camera.position.set(5,5,10);
     renderer = new THREE.WebGLRenderer();
-    var container = document.getElementById('threedvis');
-    var w = container.offsetWidth;
-    var h = container.offsetHeight;
+    var container = document.getElementById(DIV_ELEMENT_ID);
+    let w = container.offsetWidth;
+    let h = container.offsetHeight;
     renderer.setSize(w, h);
     container.appendChild( renderer.domElement );
     
     // Set up orbit
-    /*controls = new THREE.OrbitControls( camera, renderer.domElement );
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
     controls.screenSpacePanning = false;
     controls.minDistance = 0;
     controls.maxDistance = 50;
-    controls.enablePan = false;*/
+    controls.enablePan = false;
+    controls.maxPolarAngle = Math.PI / 2;
+    controls.target = ORBIT_LOOKAT_COORD;
 
     // Create points and add to scene. points is an array of THREE.Points.
     points = initializePointCloud(scene, 1000);
 
 }
 
+/** The main loop. */
 function animate() {
     requestAnimationFrame(animate);
     render();
