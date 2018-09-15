54a0a2438c7c6360c1087783f31956839a07ff93
ariane mora <ariane.mora@uq.net.au>
2018-09-15 15:23:05 +1000

    Added in drawing several charts.

--- yabbi/static/scripts/summaryVis.js
+++ yabbi/static/scripts/summaryVis.js
@@ -1,73 +1,115 @@
-// var focusChart = dc.seriesChart("#stress");
-// var overviewChart = dc.seriesChart("#stress-overview");
-var ndx, runDimension, runGroup, overviewRunDimension, overviewRunGroup;
-var experiments = [{x:1.318999999999999950,y:-3.967999999999999861,z:-8.551000000000000512},{x:1.208000000000000007,y:-6.014000000000000457,z:-8.095999999999999375},{x:1.074000000000000021,y:-9.483000000000000318,z:-7.501000000000000512}]
-  ndx = crossfilter(experiments);
-  runDimension = ndx.dimension(function(d) {return [+d.x, +d.y]; });
-  overviewRunDimension = ndx.dimension(function(d) {return [+d.x, +d.y]; });
-  runGroup = runDimension.group().reduceSum(function(d) { return +d.z; });
-  overviewRunGroup = overviewRunDimension.group().reduceSum(function(d) { return +d.z; });
-  //
-  // focusChart
-  //   .width(768)
-  //   .height(480)
-  //   .chart(function(c) { return dc.lineChart(c).curve(d3.curveCardinal).evadeDomainFilter(true); })
-  //   .x(d3.scaleLinear().domain([-10, 10]))
-  //   .brushOn(false)
-  //   .yAxisLabel("A label")
-  //   .yAxisPadding("5%")
-  //   .xAxisLabel("Run")
-  //   .elasticY(true)
-  //   .dimension(runDimension)
-  //   .group(runGroup)
-  //   .mouseZoomable(true)
-  //   .rangeChart(overviewChart)
-  //   .seriesAccessor(function(d) {
-  //     return "Expt: " + d.key[0];
-  //   })
-  //   .keyAccessor(function(d) {
-  //     return +d.key[1];
-  //   })
-  //   .valueAccessor(function(d) {
-  //     return +d.value;
-  //   })
-  //   .legend(dc.legend().x(350).y(350).itemHeight(13).gap(5).horizontal(1).legendWidth(140).itemWidth(70));
-  // focusChart.margins().left += 40;
-  //
-  // overviewChart
-  //   .width(768)
-  //   .height(100)
-  //   .chart(function(c) { return dc.lineChart(c).curve(d3.curveCardinal); })
-  //   .x(d3.scaleLinear().domain([-10,10]))
-  //   .brushOn(true)
-  //   .xAxisLabel("Run")
-  //   .clipPadding(10)
-  //   .dimension(runDimension)
-  //   .group(runGroup)
-  //   .seriesAccessor(function(d) {
-  //     return "Expt: " + d.key[0];
-  //   })
-  //   .keyAccessor(function(d) {
-  //     return +d.key[1];
-  //   })
-  //   .valueAccessor(function(d) {
-  //     return +d.value;
-  //   });
+var ndx
+var d = new Date()
+var d1 = d3.timeHour.offset(d, 2)
+var d2 = d3.timeHour.offset(d, 4)
+var experiments = [{time: d, values: [0,1], x:1.318999999999999950,y:-3.967999999999999861,z:-8.551000000000000512},{time: d1, values: [0,1], x:4.208000000000000007,y:-6.014000000000000457,z:-8.095999999999999375},{time: d2, values: [1,1], x:7.074000000000000021,y:-9.483000000000000318,z:-7.501000000000000512}]
+ndx = crossfilter(experiments)
+var colorScale = d3.scaleOrdinal().range(['#FFC3C8', '#C49BEA', '#889DD4', '#FFD7D7', '#EAC8E7', '#C5C5C5', '#97DCF1', '#C8F1EB', ]);
+
+/**
+ * Helper function to get the min time of a dataset
+ */
+getMinMax = function (experiments) {
+  let min = new Date();
+  let max = new Date();
+  for (var e in experiments) {
+    if (e.time < min) {
+      min = e.time
+    }
+    if (e.time > max) {
+      max = e.time
+    }
+  }
+  return {min: min, max: max}
+}
 
 
-  var barChart = dc.barChart("#bar-chart");
-    barChart
-      .width(768)
-      .height(480)
-      .x(d3.scaleLinear().domain([6,20]))
-      .brushOn(false)
-      .xAxisLabel("Run")
-      .yAxisLabel("This is the Y Axis!")
-      .dimension(runDimension)
-      .group(runGroup)
-      .on('renderlet', function(chart) {
-          chart.selectAll('rect').on("click", function(d) {
-              console.log("click!", d);
-          });
-      });
-  barChart.render();
+/**
+ * A Box plot function that uses the values of the experiments.
+ */
+makeBoxPlot = function (ndx, colorScale) {
+  let groupingIndex = 0
+  let boxPlot = dc.boxPlot('#box-plot')
+  let xDimension = ndx.dimension(function (d) {
+      return d.x;
+  })
+
+  let valuesGroup = xDimension.group().reduce(
+    function (p, v) {
+      // Here choose which index to use for the values
+      p.push(_.round(v.values[groupingIndex]));
+      return p;
+    },
+    function (p, v) {
+      p.splice(p.indexOf(_.round(v.values[groupingIndex])), 1);
+      return p;
+    },
+    function () {
+      return [];
+    }
+  );
+
+  boxPlot
+    .width(500)
+    .height(600)
+    .dimension(xDimension)
+    .group(valuesGroup)
+    .tickFormat(d3.format('.1f'))
+    .renderDataPoints(true)
+    .renderTitle(true)
+    .dataOpacity(0.5)
+    .margins({top: 30, right: 50, bottom: 50, left: 40})
+    .dataWidthPortion(0.5)
+    .yAxisLabel("Values")
+    .xAxisLabel("Values Grouped", 100)
+    .elasticY(true)
+    .elasticX(true)
+    .colors(colorScale)
+}
+
+
+
+/**
+ * A Line chart
+ */
+makeLineChart = function (ndx, colorScale, min, max) {
+  let groupingIndex = 0
+  let lineChart = dc.lineChart('#line-chart')
+
+  let timeDimension = ndx.dimension(function (d) {
+      return d3.timeSecond(new Date(d.time))
+  })
+
+  /**
+   * Here you are grouping based on a particular index value (i.e. the
+   * index that corrosponds to stress).
+   */
+  let timeGroup = timeDimension.group().reduceCount(function (d) {
+      return d.value[groupingIndex];
+  })
+
+  lineChart
+    .width(500)
+    .height(600)
+    .margins({top: 20, right: 50, bottom: 50, left: 40})
+    .x(d3.scaleBand())
+    .xUnits(dc.units.ordinal)
+    .brushOn(false)
+    .renderDataPoints(true)
+    .clipPadding(20)
+    .dimension(timeDimension)
+    .x(d3.scaleTime().domain([d3.timeSecond.offset(new Date(min), -2), d3.timeHour.offset(new Date(max), 2)]))
+    .round(d3.timeSecond)
+    .xUnits(d3.timeSeconds)
+    .yAxisLabel("Muscle Stress")
+    .xAxisLabel("Time", 100)
+    .group(timeGroup)
+    .colors(colorScale)
+}
+
+var extents = getMinMax(experiments)
+var min = extents.min
+var max = extents.max
+makeBoxPlot(ndx, colorScale)
+makeLineChart(ndx, colorScale, min, max)
+dc.renderAll();
