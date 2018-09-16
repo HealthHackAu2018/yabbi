9e41e893b569aa09f7763258a6c42384f07abcc8
Claudio Pizzolato <c.pizzolato@griffith.edu.au>
2018-09-16 13:12:57 +1000

    fix only the first frame was pulled

--- yabbi/blueprints/action/logic.py
+++ yabbi/blueprints/action/logic.py
@@ -17,13 +17,14 @@ class Logic:
             frame = np.empty([self.nodes.shape[0], self.nodes.shape[1]+1])
             frame[:,0:3] = self.nodes.values
             frame[:,3] = self.strains.values[i,:-1].T
-        self.data.append(frame)
+            self.data.append(frame)
+            #change the z position of some points to show changes in the plot
+            lin_strain = np.multiply(np.arange(frame.shape[0]), np.sin(6.28/200)/frame.shape[0])
+            frame[:,1] = np.add(frame[:,1], lin_strain)
         self.maxct = self.strains.shape[0]
         self.ct = 0
 
     def read_in_data(self):
-       
-        row = self.ct % self.maxct
         self.ct += 1
         self.ct = self.ct % len(self.data)
         return self.data[self.ct]
